ef59d6acc8b468767034b259d4b8dae9a244e192
DELL <riyana7734@gmail.com>
2023-10-08 00:43:02 +0545

    Added SignUp Page and It's Functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
-
+import RegisterScreen from './screens/RegisterScreen';
 
 
 const App = () => {
@@ -19,6 +19,7 @@ const App = () => {
           
           <Routes>
           <Route path='/login' element={<LoginScreen/>} />
+          <Route path='/register' element={<RegisterScreen/>} />
             <Route path='/product/:id' element={<ProductScreen />} />
             <Route path='/cart/:id?' element={<CartScreen />} />
             <Route path='/' element={<HomeScreen />} exact />
