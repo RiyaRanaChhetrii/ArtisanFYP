db727c4f7a3f0331b8b0973abf5315eb15aea4eb
DELL <riyana7734@gmail.com>
2023-11-03 21:05:40 +0545

    Shipping Screen

---
+++ frontend/src/screens/ShippingScreen.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import FormContainer from "../components/FormContainer";
+
+const ShippingScreen = () => {
+  return <div>Shipping</div>
+  
+}
+
+export default ShippingScreen
