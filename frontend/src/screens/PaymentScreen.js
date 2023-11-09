c63fd7ddfe07e4536a9714c11df3d510056a9282
DELL <riyana7734@gmail.com>
2023-11-09 09:39:41 +0545

    Payment Screen and Save Payment Method

---
+++ frontend/src/screens/PaymentScreen.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Form, Button, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
+import { savePaymentMethod } from "../action/cartAction";
+
+const PaymentScreen = () => {
+  const navigate = useNavigate();
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  if (!shippingAddress) {
+    navigate("/shipping");
+  }
+
+  const [paymentMethod, setPaymentMethod] = useState("PayPal");
+
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    navigate("/placeorder");
+  };
+
+  // Payment Method input form
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 />
+      <h1>Payment Method</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group>
+        <Form.Label as="legend">Select Method</Form.Label>
+        <Col>
+          <Form.Check
+            type="radio"
+            label="PayPal or Credit Card"
+            id="PayPal"
+            name="paymentMethod"
+            value="PayPal"
+            checked
+            onChange={(e) => setPaymentMethod(e.target.value)}
+          ></Form.Check>
+
+          <Form.Check
+            type="radio"
+            label="CashOnDelivery"
+            id="CashOnDelivery"
+            name="paymentMethod"
+            value="CashOnDelivery"
+            onChange={(e) => setPaymentMethod(e.target.value)}
+          ></Form.Check>
+        </Col>
+        </Form.Group>
+
+        <Button type="submit" variant="primary">
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default PaymentScreen;
--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -8,28 +8,29 @@ import { saveShippingAddress } from "../action/cartAction";
 
 const ShippingScreen = () => {
   const navigate = useNavigate();
-  const cart = useSelector(state => state.cart)
-  const { shippingAddress } = cart
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
 
-  const [address, setAddress] = useState(/*shippingAddress.address*/)
-  const [city, setCity] = useState(/*shippingAddress.city*/)
-  const [postalCode, setPostalCode] = useState(/*shippingAddress.postalCode*/)
-  const [country, setCountry] = useState(/*shippingAddress.country*/)
+  const [address, setAddress] = useState(/*shippingAddress.address*/);
+  const [city, setCity] = useState(/*shippingAddress.city*/);
+  const [postalCode, setPostalCode] = useState(/*shippingAddress.postalCode*/);
+  const [country, setCountry] = useState(/*shippingAddress.country*/);
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const submitHandler = (e) => {
-    e.preventDefault()
-    dispatch(saveShippingAddress({ address, city, postalCode, country}))
-    navigate('/payment')
-  }
+    e.preventDefault();
+    dispatch(saveShippingAddress({ address, city, postalCode, country }));
+    navigate("/payment");
+  };
 
   // Shipping address input form
-  return <FormContainer>
-    <CheckoutSteps step1 step2/>
-    <h1>Shipping</h1>
-    <Form onSubmit={submitHandler}>
-    <Form.Group controlId="address">
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 />
+      <h1>Shipping</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="address">
           <Form.Label>Address</Form.Label>
           <Form.Control
             type="text"
@@ -50,7 +51,7 @@ const ShippingScreen = () => {
             onChange={(e) => setCity(e.target.value)}
           ></Form.Control>
         </Form.Group>
-        
+
         <Form.Group controlId="postalCode">
           <Form.Label>Postal Code</Form.Label>
           <Form.Control
@@ -76,10 +77,9 @@ const ShippingScreen = () => {
         <Button type="submit" variant="primary">
           Continue
         </Button>
+      </Form>
+    </FormContainer>
+  );
+};
 
-    </Form>
-  </FormContainer>
-  
-}
-
-export default ShippingScreen
+export default ShippingScreen;
