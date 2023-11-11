dab02bc1988a763388d416db19b5d5ec3b3fb3d6
DELL <riyana7734@gmail.com>
2023-11-11 10:09:25 +0545

    Get Order by ID Endpoint

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -15,29 +15,44 @@ const addOrderItems = asyncHandler(async (req, res) => {
   } = req.body;
 
   // Check if there are no order items
-  if(orderItems && orderItems.length === 0) {
-    res.status(400)
-    throw new Error('No order items')
-    return
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+    return;
   } else {
-     // Create a new Order instance with the provided data
+    // Create a new Order instance with the provided data
     const order = new Order({
-        orderItems,
-        user: req.user._id, // Assuming user is attached to the request (private route)
-        shippingAddress,
-        paymentMethod,
-        itemsPrice,
-        shippingPrice,
-        totalPrice,
-    })
+      orderItems,
+      user: req.user._id, // Assuming user is attached to the request (private route)
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      shippingPrice,
+      totalPrice,
+    });
 
-     // Save the order to the database
-    const createdOrder = await order.save()
+    // Save the order to the database
+    const createdOrder = await order.save();
 
     // Respond with a JSON indicating the created order
-    res.status(201).json(createdOrder)
+    res.status(201).json(createdOrder);
   }
-
 });
 
-export { addOrderItems }
+//desc    Get order by ID
+//route   GET /api/orders/:id
+//access  Private
+const getOrderById= asyncHandler(async (req, res) => {
+  // Retrieve the order by ID and populate the 'user' field with 'name' and 'email'
+  const order = await Order.findById(req.params.id).populate('user', 'name email')
+
+  // Check if the order exists
+  if(order){
+    res.json(order) // Send the order details as a JSON response
+  } else {
+    res.status(404) // If the order is not found, set a 404 status and throw an error
+    throw new Error('Order not found') 
+  }
+});
+
+export { addOrderItems, getOrderById };
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { addOrderItems } from "../controllers/orderControllers.js";
+import { addOrderItems, getOrderById } from "../controllers/orderControllers.js";
 
 import { protect } from "../middleware/authMiddleware.js";
 
-router.route("/").post(protect, addOrderItems);
+router.route("/").post(protect, addOrderItems); // Add new order route (Private)
+router.route("/:id").get(protect, getOrderById); // Get order by ID route (Private)
 
 export default router;
