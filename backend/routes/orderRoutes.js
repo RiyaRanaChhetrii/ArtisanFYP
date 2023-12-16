a2fcc08e09359fe6ccc98228f40298e8c1e5098c
unknown <np03cs4m210046@heraldcollege.edu.np>
2023-12-16 15:54:31 +0545

    Added a controller and routes for the user orders list

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,17 @@
 import express from "express";
 const router = express.Router();
-import { addOrderItems, getOrderById, updateOrderToPaid } from "../controllers/orderControllers.js";
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  getMyOrders
+} from "../controllers/orderControllers.js";
 
 import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(protect, addOrderItems); // Add new order route (Private)
+router.route("/myorders").get(protect, getMyOrders);
 router.route("/:id").get(protect, getOrderById); // Get order by ID route (Private)
-router.route("/:id/pay").put(protect, updateOrderToPaid); 
+router.route("/:id/pay").put(protect, updateOrderToPaid);
 
 export default router;
