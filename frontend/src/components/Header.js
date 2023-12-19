b85e0c20c7f2b0645f6d8e376c978d84c59d3f44
unknown <np03cs4m210046@heraldcollege.edu.np>
2023-12-19 11:34:53 +0545

    Admin User List

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,13 +5,13 @@ import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { logout } from "../action/userAction";
 
 export const Header = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const userLogin = useSelector(state => state.userLogin)
-  const { userInfo } = userLogin
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
   const logoutHandler = () => {
-    dispatch(logout())
-  }
+    dispatch(logout());
+  };
   return (
     <header>
       <Navbar bg="dark" variant="dark" data-bs-theme="dark">
@@ -31,18 +31,34 @@ export const Header = () => {
               </Nav.Link>
             </LinkContainer>
             {userInfo ? (
-              <NavDropdown title={userInfo.name} id='username'>
-                <LinkContainer to='/profile'>
+              <NavDropdown title={userInfo.name} id="username">
+                <LinkContainer to="/profile">
                   <NavDropdown.Item>Profile</NavDropdown.Item>
                 </LinkContainer>
-                <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                <NavDropdown.Item onClick={logoutHandler}>
+                  Logout
+                </NavDropdown.Item>
               </NavDropdown>
-            ) : <LinkContainer to="/login">
-            <Nav.Link>
-              <i className="fas fa-user"></i>Sign In
-            </Nav.Link>
-          </LinkContainer> }
-
+            ) : (
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user"></i>Sign In
+                </Nav.Link>
+              </LinkContainer>
+            )}
+            {userInfo && userInfo.isAdmin && (
+              <NavDropdown title="Admin" id="adminmenu">
+                <LinkContainer to="/admin/userlist">
+                  <NavDropdown.Item>Users</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="admin/productlist">
+                  <NavDropdown.Item>Products</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="admin/orderlist">
+                  <NavDropdown.Item>Orders</NavDropdown.Item>
+                </LinkContainer>
+              </NavDropdown>
+            )}
           </Nav>
         </Container>
       </Navbar>
